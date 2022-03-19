74eb436131962439bcfc89ce0515d88e29516d88
Bruno Gouveia <brunovcg@gmail.com>
2022-03-19 08:30:12 -0300

    cc

--- categories/models.py
+++ categories/models.py
@@ -3,7 +3,6 @@ from accounts.models import User
 class Categories (models.Model):
 
     category = models.CharField(max_length=255, null=False)
-    categoryId = models.CharField(max_length=255, unique=True, null=False)
     description = models.CharField(max_length=255)
     active = models.BooleanField(null=False)
     position = models.IntegerField(null=False)
--- items/models.py
+++ items/models.py
@@ -2,7 +2,6 @@ from django.db import models
 class Items (models.Model):
 
     title = models.CharField(max_length=255, null=False)
-    itemId = models.CharField(max_length=255, unique=True, null=False)
     desc = models.CharField(max_length=255)
     active = models.BooleanField(null=False)
     position = models.IntegerField(null=False)
